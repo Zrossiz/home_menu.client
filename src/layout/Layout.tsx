056c9eb96899e1d35ac0c6af2b3ed447240f5b01
Zrossiz <soon.pluxury@mail.ru>
2024-05-28 10:15:19 +0300

    feat: added header component

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,11 @@
 import { LayoutProps } from "./Layout.props"
 import styles from './Layout.module.scss';
+import { Header } from "./Header/Header";
 
 export const Layout = ({ children }: LayoutProps) => {
     return (
         <div className={styles.wrapper}>
-            <div className={styles.header}>header</div>
+            <Header />
             <main className={styles.main}>
                 {children}
             </main>
