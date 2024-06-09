b83accb41f354ddda2c36a6facdc59e17dae397d
Zrossiz <soon.pluxury@mail.ru>
2024-06-09 22:10:06 +0300

    feat: added styles 4 header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -1,7 +1,20 @@
+import Link from 'next/link';
 import styles from './Header.module.scss';
+import Image from 'next/image';
 
 export const Header = () => {
     return (
-        <header className={styles.wrapper}>header</header>
+        <header className={styles.wrapper}>
+            <div className={styles.content}>
+                <div className={styles.logoWrapper}>
+                    <Image 
+                        src='/Logo.svg' 
+                        alt="Логотип" 
+                        width={100}
+                        height={80}
+                    />
+                </div>
+            </div>
+        </header>
     )
 }
